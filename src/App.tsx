c7207719c63cbcf8533932cd763615234d004445
Nikita Tyrkin <tyrkinn@pm.me>
2023-09-28 01:10:17 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center flex-col gap-6">
+      <h1 className="font-bold text-5xl"> Data Table Project </h1>
+      <h2 className="text-4xl"> Implement me </h2>
+    </div>
+  )
+}
+
+export default App
